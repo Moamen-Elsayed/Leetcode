b21f34f5319d716f43a92edde54e62a6bbecf20c
MoamenElsayed <47214344+Moamen-Elsayed@users.noreply.github.com>
2022-07-06 13:23:22 +0200

    Time: 1 ms (63.89%), Space: 42.2 MB (29.20%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,5 +1,20 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        
+        if(needle.length() == 0) return 0;
+        if(needle.length() > haystack.length()) return -1;
+        
+        int nlen = needle.length();
+        int hlen = haystack.length();
+        for(int i=0 ; i<= (hlen - nlen) ; i++){
+            int j = 0;
+            while(j<nlen && haystack.charAt(i + j) == needle.charAt(j))
+                j++;
+            if(j == nlen)
+                return i;
+        }
+        
+        return -1;
+            
     }
 }
