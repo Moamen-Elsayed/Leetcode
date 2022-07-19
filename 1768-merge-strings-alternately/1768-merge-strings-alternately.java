239481891f9c22cf9ecb7cf3033f8e5e9a300dbe
MoamenElsayed <47214344+Moamen-Elsayed@users.noreply.github.com>
2022-07-19 08:58:23 +0200

    Time: 1 ms (93.06%), Space: 42 MB (79.42%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,20 +1,12 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int i=0 , j=0;
+        int maxlen = Math.max(word1.length(), word2.length());
         StringBuilder sb = new StringBuilder("");
-        while(i < word1.length() && j < word2.length()){
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(j));
-            i++; j++;
+        
+        for(int i=0 ; i<maxlen ; i++){
+            if(i < word1.length()) sb.append(word1.charAt(i));
+            if(i < word2.length()) sb.append(word2.charAt(i));
         }
-        
-        System.out.println(i); System.out.println(j);
-        
-        // if(i < word1.length()) sb.append(word1.substring(i));
-        // if(j < word2.length()) sb.append(word1.substring(j));
-        
-        while(i <  word1.length()) {sb.append(word1.charAt(i)); i++;}
-        while(j < word2.length()) {sb.append(word2.charAt(j)); j++;}
         
         return sb.toString();
     }
